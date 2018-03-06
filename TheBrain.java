f71372f4c3c578b0b0a83876676580738e45a01d
cashyus <mdolic1@dmacc.edu>
2018-03-06 11:29:33 -0600

    this is my commit message

--- TheBrain.java
+++ TheBrain.java
@@ -5,6 +5,6 @@ public class TheBrain {
 	public static void main(String[] args) {
 		System.out.println("does it work?");
 		System.out.println("So far, so good...");
-		System.out.println("Thanks");
+		System.out.println("its pretty good...Thanks");
 	}
 }
