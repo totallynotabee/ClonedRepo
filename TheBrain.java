00d7378797b6f519511cd56b7d89ed9eac991dbd
Jonah <jjzirkle@dmacc.edu>
2018-03-07 16:56:04 -0600

    One step closer to world domination, Pinky!

--- TheBrain.java
+++ TheBrain.java
@@ -4,7 +4,7 @@ public class TheBrain {
 
 	public static void main(String[] args) {
 		System.out.println("does it work?");
-		System.out.println("So far, so good...");
+		System.out.println("So far, so good!");
 		// add more
 	}
 }
